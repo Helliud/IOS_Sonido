82a96bc029be3d74bb7dc76cf8b8dc63023f8d92
Alumno <ulsa@JesusCorral.local>
2019-08-23 14:24:41 -0700

    Sonido y cambio de pantalla

---
+++ Sonido/SegundaPantalla_Controller.swift
@@ -0,0 +1,18 @@
+//
+//  SegundaPantalla_Controller.swift
+//  Sonido
+//
+//  Created by Alumno on 8/23/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SegundaPantalla_Controller : UIViewController{
+    @IBAction func doTapRegresar(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
---
+++ Sonido/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Sonido
+//
+//  Created by Alumno on 21/08/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    var player : AVAudioPlayer?
+    var playerMusicaFondo : AVAudioPlayer?
+    var reproduccion = false
+    
+    @IBAction func doTapReproducir(_ sender: Any) {
+        if reproduccion {
+            let pathMusicaFondo = Bundle.main.path(forResource: "musicaFondo.wav", ofType: nil)
+            let urlMusicaFondo = URL(fileURLWithPath : pathMusicaFondo!)
+        
+            do{
+                player = try AVAudioPlayer(contentsOf : urlMusicaFondo)
+            } catch {
+                
+            }
+        }
+        else{
+            reproduccion = true
+        }
+        
+        
+        if player != nil { //Nil: nulo
+            
+            player?.play()
+        
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let path = Bundle.main.path(forResource: "musica.wav", ofType: nil)
+        let url = URL(fileURLWithPath: path!)
+        
+        let pathMusicaFondo = Bundle.main.path(forResource : "musicaFondo.wav", ofType: nil)
+        let urlMusicaFondo = URL(fileURLWithPath: pathMusicaFondo!)
+        
+        do {
+            
+            player = try AVAudioPlayer(contentsOf: url)
+            playerMusicaFondo = try AVAudioPlayer(contentsOf: urlMusicaFondo)
+            //playerMusicaFondo?.play()
+            
+        } catch {
+            
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
